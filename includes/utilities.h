5505c8d1e9150fd71b58b7d4e2eb2fada9bdcb66
DerinSozen <67414435+DerinSozen@users.noreply.github.com>
2023-05-21 15:51:33 +0400

    Command line UI

---
+++ includes/utilities.h
@@ -0,0 +1,12 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
+#include <string>
+#include <stdexcept>
+
 #include "includes/utilities.h"
-int main() {
+
+int main(int argc, char* const argv[]) {
     std::string passphrase;
-    std::string publicKeyFile = "";
     std::string message;
 
-    // Get passphrase from user
-    std::cout << "Enter passphrase: ";
-    std::getline(std::cin, passphrase);
+    if(argc != 2){
+        std::cout << "invalid usage, refer to documentation" << std::endl;
+        return 1;
+    }
 
-    // Get public key file from user
-    std::cout << "Enter public key file name (e.g., public.pem): ";
-    std::getline(std::cin, publicKeyFile);
+    if(strcmp(argv[1], "--encrypt") == 0){
+        // Get passphrase from user
+        std::cout << "Enter passphrase: ";
+        std::getline(std::cin, passphrase);
 
-    // Get message to encrypt from user
-    std::cout << "Enter message to encrypt: ";
-    std::getline(std::cin, message);
+        // Get message to encrypt from user
+        std::cout << "Enter message to encrypt: ";
+        std::getline(std::cin, message);
+
+        // Encrypt the message and display
+        std::string encryptedMessage = "test";
+        std::cout << "Encrypted message: " << encryptedMessage << std::endl;
+        return 0;
+    }
+    if(strcmp(argv[1], "--decrypt") == 0){
+        // Get passphrase from user
+        std::cout << "Enter passphrase: ";
+        std::getline(std::cin, passphrase);
+
+        // Get message to encrypt from user
+        std::cout << "Enter message to encrypt: ";
+        std::getline(std::cin, message);
+
+        // Encrypt the message and display
+        std::string encryptedMessage = "test";
+        std::cout << "Encrypted message: " << encryptedMessage << std::endl;
+        return 0;
+    }
 
     return 0;
 }
